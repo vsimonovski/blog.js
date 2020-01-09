8e81df17da289a79fec5608cfaaa607c03154f15
Vladimir Simonovski <vsimonovski@outlook.com>
2020-01-09 21:56:38 +0100

    :pencil: docs: git hooks article

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,7 @@
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
+// command line style
+import 'prismjs/plugins/command-line/prism-command-line.css'
+
 import './src/utils/global.scss'
